60dace844fdd20152b7fc86bb7c7b6d1c40c1e31
Kousalya531 <kousalyadasari531@gmail.com>
2022-04-20 18:43:05 -0500

    something

--- controllers/tea.js
+++ controllers/tea.js
@@ -12,21 +12,23 @@ exports.tea_list = async function(req, res) {
     }
    };
 
-//exports.tea_list = function(req, res) {
+//exports tea_list = function(req, res) {
  //res.send('NOT IMPLEMENTED: tea list');
 //};
 // for a specific tea.
-// for a specific tea. 
-exports.tea_detail = async function(req, res) { 
-    console.log("detail"  + req.params.id) 
-    try { 
-        result = await tea.findById( req.params.id) 
-        res.send(result) 
-    } catch (error) { 
-        res.status(500) 
-        res.send(`{"error": document for id ${req.params.id} not found`); 
-    } 
-}; 
+exports.tea_detail = async function(req, res) {
+ //res.send('NOT IMPLEMENTED: tea detail: ' + req.params.id);
+ console.log("detail"  + req.params.id) 
+ try { 
+     result = await tea.findById( req.params.id) 
+     res.send(result) 
+ } catch (error) { 
+     res.status(500) 
+     console.log("test--")
+     res.send(`{"error": document for id ${req.params.id} not found`); 
+ } 
+};
+// Handle tea create on POST.
 // Handle tea create on POST.
 exports.tea_create_post = async function(req, res) {
     console.log(req.body)
@@ -34,7 +36,7 @@ exports.tea_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"tea_type":"goat", "cost":12, "size":"large"}
     document.tea_flavour = req.body.tea_flavour;
     document.tea_size = req.body.tea_size;
     document.tea_cost = req.body.tea_cost;
@@ -48,18 +50,37 @@ exports.tea_create_post = async function(req, res) {
     }
    };
 // Handle tea delete form on DELETE.
-exports.tea_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: tea delete DELETE ' + req.params.id);
-};
-//Handle Costume update form on PUT. 
+//exports.tea_delete = function(req, res) {
+ //res.send('NOT IMPLEMENTED: tea delete DELETE ' + req.params.id);
+//};
+// Handle tea delete on DELETE. 
+// Handle Costume delete on DELETE.
+exports.tea_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await tea.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+// Handle tea update form on PUT.
+//exports.tea_update_put = function(req, res) {
+ //res.send('NOT IMPLEMENTED: tea update PUT' + req.params.id);
+//};
+
+//Handle tea update form on PUT. 
 exports.tea_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
 ${JSON.stringify(req.body)}`) 
     try { 
         let toUpdate = await tea.findById( req.params.id) 
+        console.log(req.body);
         // Do updates of properties 
         if(req.body.tea_flavour)  
-               toUpdate.tea_flavour= req.body.tea_flavour; 
+               toUpdate.tea_flavour = req.body.tea_flavour; 
         if(req.body.tea_size) toUpdate.tea_size = req.body.tea_size; 
         if(req.body.tea_cost) toUpdate.tea_cost = req.body.tea_cost; 
         let result = await toUpdate.save(); 
@@ -67,16 +88,15 @@ ${JSON.stringify(req.body)}`)
         res.send(result) 
     } catch (err) { 
         res.status(500) 
-        res.send(`{"error": ${err}: Update for id ${req.params.id} 
-failed`); 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
     } 
 }; 
-
 
 // VIEWS
 // Handle a show all view
 exports.tea_view_all_Page = async function(req, res) {
     try{
+        console.log('test'+tea.find())
     thetea = await tea.find();
     res.render('tea', { title: 'tea Search Results', results: thetea });
     }
@@ -86,15 +106,58 @@ exports.tea_view_all_Page = async function(req, res) {
     }
    };
 
-   // Handle Costume delete on DELETE.
-exports.costume_delete = async function(req, res) {
-    console.log("delete " + req.params.id)
-    try {
-    result = await tea.findByIdAndDelete( req.params.id)
-    console.log("Removed " + result)
-    res.send(result)
-    } catch (err) {
+   // Handle a show one view with id specified by query
+exports.tea_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await tea.findById( req.query.id)
+    res.render('teadetail',
+   { title: 'tea Detail', toShow: result });
+    }
+    catch(err){
     res.status(500)
-    res.send(`{"error": Error deleting ${err}}`);
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.tea_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('teacreate', { title: 'tea Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.tea_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await tea.findById(req.query.id)
+    res.render('teaupdate', { title: 'tea Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle a delete one view with id from query
+exports.tea_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await tea.findById(req.query.id)
+    res.render('teadelete', { title: 'tea Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
    };
--- routes/tea.js
+++ routes/tea.js
@@ -4,3 +4,15 @@ var router = express.Router();
 /* GET tea */
 router.get('/', tea_controlers.tea_view_all_Page );
 module.exports = router;
+
+/* GET detail costume page */
+router.get('/detail', tea_controlers.tea_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', tea_controlers.tea_create_Page);
+
+/* GET create update page */
+router.get('/update', tea_controlers.tea_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', tea_controlers.tea_delete_Page);
