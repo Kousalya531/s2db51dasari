fc245d5aa8cf303290ce111f60e0d75e3cf56987
Kousalya531 <kousalyadasari531@gmail.com>
2022-04-27 16:33:06 -0500

    something

--- models/tea.js
+++ models/tea.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose") 
 const teaSchema = mongoose.Schema({ 
     tea_flavour: String,
-    tea_size: String,
-    tea_cost: Number
+    tea_size: {type: String, 
+        minLength: 1 , maxLength: 2456},
+    tea_cost: {
+        type:Number, min: 1, max: 231}
+
+
 }) 
  
 module.exports = mongoose.model("Mobile", teaSchema) 
--- routes/tea.js
+++ routes/tea.js
@@ -19,12 +19,12 @@ router.get('/', tea_controlers.tea_view_all_Page );
 router.get('/detail', tea_controlers.tea_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', tea_controlers.tea_create_Page);
+router.get('/create',secured, tea_controlers.tea_create_Page);
 
 /* GET create update page */
 router.get('/update',secured, tea_controlers.tea_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', tea_controlers.tea_delete_Page);
+router.get('/delete', secured, tea_controlers.tea_delete_Page);
 
 module.exports = router;
